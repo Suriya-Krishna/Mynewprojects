61790c599a4df1a9194f4514adeead4d582db210
Suriya Krishna.B.S <90369580+Suriya-Krishna@users.noreply.github.com>
2021-09-28 18:09:32 +0530

    Update Db1.py

--- Db1.py
+++ Db1.py
@@ -1,19 +1,38 @@
 import sqlite3
+def db_connect():
+    conn = sqlite3.connect('Car_new.db')
+    return conn
 
-conn = sqlite3.connect('Cars_new.db')
-query = """CREATE TABLE CARS(NAME_OF_CAR CHAR(60) NOT NULL, NAME_OF_OWNER CHAR(100) NOT NULL)"""
-conn.execute(query)
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HYUNDAI','RAM')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('TOYOTA','SAM')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('DUSTER','PREM')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('SUZUKI','HARI')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HONDA','NILA')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HYUNDAI','DEV')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('BMW','KUMAR')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('WAGON','LILLY')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HONDA','SAMMY')")
-conn.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('SUZUKI','KIRAN')")
-conn.commit()
-cursor = conn.execute("SELECT * from CARS")
-print(cursor.fetchall())
+def create_table():
+      con = db_connect()
+      cur = con.cursor()
+      query = """CREATE TABLE CARS(NAME_OF_CAR CHAR(60) NOT NULL, NAME_OF_OWNER CHAR(100) NOT NULL)"""
+      cur.execute(query)
+      con.commit()
+      con.close()
+
+def add_data():
+    con = db_connect()
+    cur = con.cursor()
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HYUNDAI','RAM')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('TOYOTA','SAM')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('DUSTER','PREM')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('SUZUKI','HARI')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HONDA','NILA')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HYUNDAI','DEV')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('BMW','KUMAR')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('WAGON','LILLY')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('HONDA','SAMMY')")
+    cur.execute("INSERT INTO CARS(NAME_OF_CAR,NAME_OF_OWNER)""VALUES('SUZUKI','KIRAN')")
+    con.commit()
+    con.close()
+
+
+con = db_connect()
+cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS CARS")
+create_table()
+add_data()
+cur.execute("SELECT * from CARS")
+print(cur.fetchall())
 conn.close()
